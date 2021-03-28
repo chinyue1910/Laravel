b4f03b27a8459b607a8bb0010b24dc54b6c52091
chinyue1910 <asd874631@gmail.com>
2021-03-28 14:55:26 +0800

    init

---
+++ app/Exceptions/Handler.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Exceptions;
+
+use App\Traits\ApiResponseTrait;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Throwable;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Symfony\Component\HttpFoundation\Response;
+
+class Handler extends ExceptionHandler
+{
+    use ApiResponseTrait;
+
+    /**
+     * A list of the exception types that are not reported.
+     *
+     * @var array
+     */
+    protected $dontReport = [
+        //
+    ];
+
+    /**
+     * A list of the inputs that are never flashed for validation exceptions.
+     *
+     * @var array
+     */
+    protected $dontFlash = [
+        'current_password',
+        'password',
+        'password_confirmation',
+    ];
+
+    /**
+     * Register the exception handling callbacks for the application.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->reportable(function (Throwable $e) {
+            //
+        });
+    }
+
+    public function render($request, Throwable $e)
+    {
+        if($request->expectsJson()){
+            if($e instanceof ModelNotFoundException){
+                return $this->errorResponse(
+                    '找不到資源',
+                    Response::HTTP_NOT_FOUND
+                );
+            }
+            if($e instanceof NotFoundHttpException){
+                return $this->errorResponse(
+                    '無法找到此網址',
+                    Response::HTTP_NOT_FOUND
+                );
+            }
+            if($e instanceof MethodNotAllowedHttpException){
+                return $this->errorResponse(
+                    $e->getMessage(),
+                    Response::HTTP_METHOD_NOT_ALLOWED
+                );
+            }
+        }
+
+        return parent::render($request, $e);
+    }
+}
